f5ed470a9aa0eb82f590a44a3635e58823ddd270
thuy tran van <thuytranvan.gl@gmail.com>
2023-05-25 01:07:01 +0700

    update smth

--- handmade.c
+++ handmade.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<SDL.h>
 
-#include "handmade.h"
+#include "./handmade.h"
 
 void check_code(char code) {
     if (code < 0) {
@@ -31,9 +31,12 @@ int main(int argc, char** argv) {
     SDL_Surface* surface = check_ptr(SDL_GetWindowSurface(window));
     SDL_Texture* texture = check_ptr(SDL_CreateTextureFromSurface(renderer,
                 surface));
+    SDL_Surface* sf_hello_world = check_ptr(SDL_LoadBMP("./hello-world.bmp"));
+    SDL_Rect viewport = {0, 0, 25, 25};
+    SDL_Rect sqr01 = {0, 0, 50, 50};
+    SDL_Rect sqr02 = {50, 50, 50, 50};
     SDL_Event evt;
-    short red = 50;
-    short blue = 100;
+    char redraw = 1;
     for (;;) {
         SDL_PollEvent(&evt);
         if (evt.type == SDL_QUIT) {
@@ -43,13 +46,24 @@ int main(int argc, char** argv) {
         if (evt.type == SDL_WINDOWEVENT
                 && evt.window.event == SDL_WINDOWEVENT_RESIZED)
         {
-            red += 10;
-            blue += 5;
+            // viewport.w += 1;
+            // viewport.h += 1;
+            // surface = check_ptr(SDL_GetWindowSurface(window));
+            redraw = 1;
         }
-        check_code(SDL_SetRenderDrawColor(renderer, red, 44, blue, 255));
-        check_code(SDL_RenderClear(renderer));
+        // SDL_BlitSurface(landscape, &viewport, surface, NULL);
+        // SDL_UpdateWindowSurface(window);
+        if (redraw) {
+            check_code(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
+            check_code(SDL_RenderClear(renderer));
+            check_code(SDL_SetRenderDrawColor(renderer, 255, 10, 5, 255));
+            check_code(SDL_RenderFillRect(renderer, &sqr01));
+            check_code(SDL_SetRenderDrawColor(renderer, 10, 255, 5, 255));
+            check_code(SDL_RenderFillRect(renderer, &sqr02));
+            redraw = 0;
+        }
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        SDL_Delay(12);
     }
 
 	SDL_DestroyWindow(window);
