497d8c326a738b9a4d3b9b0fc0970ea0c1c48ab6
thuy tran van <thuytranvan.gl@gmail.com>
2023-06-03 01:14:03 +0700

    snake can runs and turns

--- handmade.c
+++ handmade.c
@@ -1,26 +1,17 @@
-#include<stdio.h>
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_image.h>
-
-// CONFIGS
-#define WINDOW_TITLE   "Handmade"
-#define WINDOW_WIDTH    1080
-#define WINDOW_HEIGHT   720
-#define FRAME_RATE   1000/60
-#define ICON_SIZE 32
-
-// EVENTS
-#define HMD_EVENT_QUIT 0
-
-#define local_persist static
-#define global_variable static
-
-
-const int C_WINDOW_FLAGS = SDL_WINDOW_RESIZABLE;
+#include "handmade.h"
 
 global_variable char running = 1;
 global_variable SDL_Texture *arts_texture = NULL;
-global_variable SDL_Rect snake_body[] = {{0, 0, 32, 32}, {32, 0, 32, 32}, {}};
+global_variable Snake snake = {DirectionEast};
+const SDL_Rect wall_tile = {
+    ICON_SIZE*2,
+    ICON_SIZE*2,
+    ICON_SIZE,
+    ICON_SIZE
+};
+const SDL_Rect snake_tile = {ICON_SIZE, ICON_SIZE*2, ICON_SIZE, ICON_SIZE};
+const SDL_Rect head_tile = {ICON_SIZE, ICON_SIZE*3, ICON_SIZE, ICON_SIZE};
+const int snake_len = BOARD_SIZE * BOARD_SIZE;
 
 void scc(char code) {
     if (code < 0) {
@@ -29,8 +20,7 @@ void scc(char code) {
     }
 }
 
-template <typename T>
-T* scp(T *ptr) {
+template <typename T> T* scp(T *ptr) {
     if (ptr == NULL) {
         fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
         exit(1);
@@ -38,31 +28,41 @@ T* scp(T *ptr) {
     return ptr;
 }
 
-void prepare_scene(SDL_Renderer *renderer) {
-    scc(SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255));
-    scc(SDL_RenderFillRect(renderer, NULL));
-}
-
-void render_wall(SDL_Renderer *renderer) {
-    SDL_Rect src_tile = {ICON_SIZE*2, ICON_SIZE*2, ICON_SIZE, ICON_SIZE};
+void prepare_wall(SDL_Renderer *renderer) {
     SDL_Rect target_tile = {0, 0, 32, 32};
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
         target_tile.x = i * 32;
-        for (int j = 0; j < 20; ++j) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
             target_tile.y = j * 32;
             if (i == 0 || j == 0 || i == 19 || j == 19) {
-                SDL_RenderCopy(renderer, arts_texture, &src_tile, &target_tile);
+                SDL_RenderCopy(renderer, arts_texture, &wall_tile, &target_tile);
             }
         }
     }
 }
 
-void render_snake(SDL_Renderer *renderer) {
-    SDL_Rect src_tile = {ICON_SIZE, ICON_SIZE*2, ICON_SIZE, ICON_SIZE};
-    int snake_len = sizeof(snake_body)/sizeof(SDL_Rect);
+void prepare_snake(SDL_Renderer *renderer) {
+    SDL_Rect rect;
+    rect.w = ICON_SIZE;
+    rect.h = ICON_SIZE;
     for (int i = 0; i < snake_len; ++i) {
-        SDL_RenderCopy(renderer, arts_texture, &src_tile, &snake_body[i]);
+        if (snake.body[i].x == 0)
+            break;
+
+        rect.x = snake.body[i].x * ICON_SIZE;
+        rect.y = snake.body[i].y * ICON_SIZE;
+        if (i == 0) 
+            SDL_RenderCopy(renderer, arts_texture, &head_tile, &rect);
+        else
+            SDL_RenderCopy(renderer, arts_texture, &snake_tile, &rect);
     }
+}
+
+void prepare_scene(SDL_Renderer *renderer) {
+    scc(SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255));
+    scc(SDL_RenderFillRect(renderer, NULL));
+    prepare_wall(renderer);
+    prepare_snake(renderer);
 }
 
 void do_render(SDL_Renderer *renderer) {
@@ -70,9 +70,38 @@ void do_render(SDL_Renderer *renderer) {
 }
 
 void snake_forward() {
-    int snake_len = sizeof(snake_body)/sizeof(SDL_Rect);
-    for (int i = 0; i < snake_len; ++i) {
-        snake_body[i].x += 32;
+    int tail = -1;
+    for (int i = 0; i < snake_len; i++) {
+        if (snake.body[i].x == 0) {
+            tail = i-1;
+            break;
+        }
+    }
+
+    SDL_Point head = snake.body[0];
+    for (;tail > 0; --tail) {
+        snake.body[tail].x = snake.body[tail-1].x;
+        snake.body[tail].y = snake.body[tail-1].y;
+    }
+    switch(snake.d)
+    {
+        case DirectionEast:
+        {
+            snake.body[0].x = head.x + 1;
+        } break;
+        case DirectionWest:
+        {
+            snake.body[0].x = head.x - 1;
+        } break;
+        case DirectionNorth:
+        {
+            snake.body[0].y = head.y - 1;
+        } break;
+        case DirectionSouth:
+        {
+            snake.body[0].y = head.y + 1;
+        } break;
+        default: break;
     }
 }
 
@@ -99,20 +128,24 @@ void window_event_handler(SDL_Window *window, SDL_Renderer *renderer,
             switch(event->key.keysym.sym) {
                 case SDLK_a:
                 {
-                    snake_body[0].x -= 32;
+                    snake.d = DirectionWest;
                 } break;
                 case SDLK_d:
                 {
-                    snake_forward();
+                    snake.d = DirectionEast;
                 } break;
                 case SDLK_w:
                 {
-                    snake_body[0].y -= 32;
+                    snake.d = DirectionNorth;
                 } break;
                 case SDLK_s:
                 {
-                    snake_body[0].y += 32;
+                    snake.d = DirectionSouth;
                 } break;
+                // case SDLK_SPACE:
+                // {
+                //     snake_forward();
+                // } break;
                 default: break;
             }
         } break;
@@ -126,19 +159,25 @@ void init_sdl() {
 
 int main(int argc, char** argv) {
     init_sdl();
+    for (int i = 0; i < 5; ++i) {
+        snake.body[i].x = 5 - i;
+        snake.body[i].y = 2;
+    }
 
     SDL_Window* window = scp(SDL_CreateWindow(WINDOW_TITLE,
                 SDL_WINDOWPOS_CENTERED,
                 SDL_WINDOWPOS_CENTERED,
                 WINDOW_WIDTH,
                 WINDOW_HEIGHT,
-                C_WINDOW_FLAGS));
+                SDL_WINDOW_RESIZABLE));
     SDL_Renderer* renderer = scp(SDL_CreateRenderer(window, -1,
                 SDL_RENDERER_ACCELERATED));
     arts_texture = scp(SDL_CreateTextureFromSurface(
                 renderer,
                 IMG_Load("./fantasy-tileset.png"))); 
     SDL_Event event;
+    float speed = 1.3;
+    Uint32 timeout = SDL_GetTicks() + speed * 100;
     while(running) {
         while(SDL_PollEvent(&event)) {
             if (event.type == SDL_USEREVENT) {
@@ -150,10 +189,13 @@ int main(int argc, char** argv) {
         }
 
         prepare_scene(renderer);
-        render_wall(renderer);
-        render_snake(renderer);
         do_render(renderer);
         SDL_Delay(FRAME_RATE);
+
+        if (SDL_TICKS_PASSED(SDL_GetTicks(), timeout)) {
+            timeout = SDL_GetTicks() + speed * 100;
+            snake_forward();
+        }
     }
 
     SDL_Log("Q U I T");
--- handmade.h
+++ handmade.h
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
 
+// CONFIGS
+#define WINDOW_TITLE     "Handmade"
+#define WINDOW_WIDTH     1080
+#define WINDOW_HEIGHT    720
+#define FRAME_RATE       1000/60
+#define ICON_SIZE        32
+#define BOARD_SIZE       20
+
+// EVENTS
+#define HMD_EVENT_QUIT 0
+
+#define local_persist static
+#define global_variable static
+
+enum Direction {
+    DirectionEast,
+    DirectionNorth,
+    DirectionSouth,
+    DirectionWest
+};
+
+typedef struct {
+    Direction d;
+    SDL_Point body[BOARD_SIZE * BOARD_SIZE];
+} Snake;
